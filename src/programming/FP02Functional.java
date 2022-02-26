3fb79cfc1e83bceb58031f3293b4a42a297c0ad8
amitu2016 <amitu2016@gmail.com>
2022-02-26 18:50:22 +0530

    reduce exercises

--- src/programming/FP02Functional.java
+++ src/programming/FP02Functional.java
@@ -6,19 +6,38 @@ public class FP02Functional {
 
 	public static void main(String[] args) {
 		List<Integer> numbers = List.of(12, 9, 4, 7, 3, 2, 12, 8, 33, 22, 9, 2);
-		int sum = addListNumbers(numbers);
-		System.out.println(sum);
-	}
-	
-	public static int add(int a, int b) {
-		return a + b;
+		// int sum = addListNumbers(numbers);
+		// int sumOfSquares = sumOfSquares(numbers);
+		//int cubeOfSquares = cubeOfSquares(numbers);
+		int sumOfOdd = sumOfOdd(numbers);
+		System.out.println(sumOfOdd);
 	}
 
 	private static int addListNumbers(List<Integer> numbers) {
 
-	return	numbers.stream()
-			.reduce(0, FP02Functional::add);
+		return numbers.stream()
+				// .reduce(0, FP02Functional::add); //using method reference
+				// .reduce(0, (x,y) -> x + y); // using lambda expression
+				.reduce(0, Integer::sum); // using predefined method of class Integer
+	}
+
+	private static int sumOfSquares(List<Integer> numbers) {
+
+		return numbers.stream().map(x -> x * x).reduce(0, Integer::sum);
 
 	}
+
+	private static int cubeOfSquares(List<Integer> numbers) {
+
+		return numbers.stream().map(x -> x * x * x).reduce(0, Integer::sum);
+
+	}
+	
+	private static int sumOfOdd(List<Integer> numbers) {
+
+		return numbers.stream().filter(x -> x % 2 != 0).reduce(0, Integer::sum);
+				
+	}
+	
 
 }
